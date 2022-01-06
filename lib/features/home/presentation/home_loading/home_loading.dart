e0d5d90736f724e9b62b879d8dfb4b59e06f1daf
Pedro Lacerda <placerdadacosta@gmail.com>
2022-01-06 16:49:30 -0400

    🔧  Creates home_loading folder

--- lib/features/home/presentation/home_loading/home_loading.dart
+++ lib/features/home/presentation/home_loading/home_loading.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sw_vehicles_pro/features/home/presentation/home_loading_card.dart';
+import 'package:sw_vehicles_pro/features/home/presentation/home_loading/home_loading_card.dart';
 
 class HomeLoading extends StatelessWidget {
   const HomeLoading({Key? key}) : super(key: key);
--- lib/features/home/presentation/home_statehandler.dart
+++ lib/features/home/presentation/home_statehandler.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sw_vehicles_pro/core/components/helpers/pallete.dart';
 import 'package:sw_vehicles_pro/core/components/widgets/app_bar.dart';
 import 'package:sw_vehicles_pro/features/home/application/home_bloc.dart';
-import 'package:sw_vehicles_pro/features/home/presentation/home_loading.dart';
+import 'package:sw_vehicles_pro/features/home/presentation/home_loading/home_loading.dart';
 
 class HomeStateHandler extends StatelessWidget {
   const HomeStateHandler({Key? key}) : super(key: key);
